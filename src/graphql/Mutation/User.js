407abdb2af99175995c25fe869ee846c767d9acc
Katherine McPhie <katherinemcphie@college.harvard.edu>
2021-01-20 12:36:58 -0800

    updated user mutation with addMoney and changePassword

--- src/graphql/Mutation/User.js
+++ src/graphql/Mutation/User.js
@@ -14,26 +14,44 @@ const createUser = async (obj, { input: { username, email, password, age, dateJo
     return { create }
 }
 
-const updateUser = async (obj, { input: { id, username, email, password, age, dateJoined, money, bio, addressId }}) => {
-    const userExists = await User.query().findById({ id })
-    if (!userExists) {
-        throw new UserInputError('User does not exist')
-    }
-    const update = await User.query().findById({ id }).patch({
-        username,
-        email,
-        password,
-        age,
-        dateJoined,
-        money,
-        bio,
-        addressId
-      })
-    return { update }
+const addMoney = async (obj, { input: { id, username, email, password, age, dateJoined, money, bio, addressId }}) => {
+  const userExists = await User.query().findById({ id })
+  if (!userExists) {
+      throw new UserInputError('User does not exist')
+  }
+  const update = await User.query().findById({ id }).patch({
+      username,
+      email,
+      password,
+      age,
+      dateJoined,
+      money,
+      bio,
+      addressId
+  })
+  return { update }
+}
+
+const changePassword = async (obj, { input: { id, username, email, password, age, dateJoined, money, bio, addressId }}) => {
+  const userExists = await User.query().findById({ id })
+  if (!userExists) {
+      throw new UserInputError('User does not exist')
+  }
+  const update = await User.query().findById({ id }).patch({
+      username,
+      email,
+      password,
+      age,
+      dateJoined,
+      money,
+      bio,
+      addressId
+  })
+  return { update }
 }
 
 const resolver = {
-    Mutation: { createUser, updateUser },
+    Mutation: { createUser, addMoney, changePassword },
   }
 
 module.exports = resolver
