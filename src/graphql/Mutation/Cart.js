a8ad52eeccac625ab0b3adbc43a23da1b84dac8a
ashcodes101010 <ashleighcoltman@college.harvard.edu>
2021-01-21 10:09:38 -0500

    cleanup

--- src/graphql/Mutation/Cart.js
+++ src/graphql/Mutation/Cart.js
@@ -3,38 +3,43 @@ const Item = require('../../models/Item')
 const User = require('../../models/User')
 
 const addCartItem = async (obj, { input: { userId, itemId } }) => {
-    const itemExists = await Item.query().findById({ itemId })
-    if (!itemExists) {
-        throw new UserInputError('Item does not exist')
-    }
-    const userExists = await User.query().findById({ userId })
-    if (!userExists) {
-        throw new UserInputError('User does not exist')
-    }
+  const itemExists = await Item.query().findById(itemId)
+  if (!itemExists) {
+    throw new Error('Item does not exist')
+  }
+  const userExists = await User.query().findById(userId)
+  if (!userExists) {
+    throw new Error('User does not exist')
+  }
 
-    const addToCart = await Cart.query().insert({
-        userId,
-        itemId
-    })
+  const addToCart = await Cart.query().insert({
+    userId,
+    itemId,
+  }).returning('*')
 
-    return { addToCart }
+  return addToCart
 }
 
 const removeCartItem = async (obj, { input: { userId, itemId } }) => {
-    const itemExists = await Cart.query().where('userId', userId).andWhere('itemId', itemId)
-    if (!itemExists) {
-        throw new UserInputError('Item does not exist')
-    }
-    const userExists = await User.query().findById({ userId })
-    if (!userExists) {
-        throw new UserInputError('User does not exist')
-    }
-    const removeFromCart = await Cart.query().delete().where('userId', userId).andWhere('itemId', itemId)
-    return { removeFromCart }
+  const itemExists = await Cart.query().where('userId', userId).andWhere('itemId', itemId)
+  if (!itemExists) {
+    throw new Error('Item does not exist')
+  }
+  const userExists = await User.query().findById(userId)
+  if (!userExists) {
+    throw new Error('User does not exist')
+  }
+  const removeFromCart = await Cart
+    .query()
+    .delete()
+    .where('userId', userId)
+    .andWhere('itemId', itemId)
+    .returning('*')
+  return removeFromCart
 }
 
 const resolver = {
-    Mutation: { addCartItem, removeCartItem },
-  }
+  Mutation: { addCartItem, removeCartItem },
+}
 
 module.exports = resolver
